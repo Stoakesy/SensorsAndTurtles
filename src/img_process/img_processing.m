c73f483d2aadc0e8f47287dfe4dcd39730ebd32c
Alex <alexoakes82@gmail.com>
2020-10-05 11:57:21 +1100

    added exception case and commented code

--- src/img_process/img_processing.m
+++ src/img_process/img_processing.m
@@ -1,58 +1,49 @@
-
+%load the calibration data of the real sense camera
 load('Calib_Results.mat', 'fc', 'cc', 'nx', 'ny');
-
+%create the camera intrinsicts values from calibration data
 intrinsics = cameraIntrinsics(fc', cc', [ny nx]);
 
-tagSize = 0.128;
+makerSize = 0.128; %size of marker in m
 
-worldPoints = [0 0 0; tagSize/2 0 0; 0 tagSize/2 0; 0 0 tagSize/2];
+worldPoints = [0 0 0; makerSize/2 0 0; 0 makerSize/2 0; 0 0 makerSize/2];
+%create ros publisher for pose of front robot
+posePub = rospublisher('/target_pose', 'geometry_msgs/Pose');
+%create ros publisher for camera on tb
+rosImg = rossubscriber('/camera/color/image_raw');
 
-posepub = rospublisher('/target_pose', 'geometry_msgs/Pose');
+pause(2); %allow for connectionof publisher and subscriber
+%enable ros message
+poseMsg = rosmessage(posePub);
 
-rosimg = rossubscriber('/camera/color/image_raw');
-
-pause(2);
-
-posemsg = rosmessage(posepub);
-
-while(1)
-    imgdata = receive(rosimg, 5);
+while (1)
+    %recieve incoming ros message of camera image
+    imgData = receive(rosImg, 5);
+    %convert from ros image format to Matlab image format
+    img = readImage(imgData);
+    %apply camera intrinsics data to image
+    img = undistortImage(img,intrinsics,"OutputView","same");
+    %read the image to find april tag
+    [id,loc,pose] = readAprilTag(img,"tag36h11",intrinsics,makerSize);
     
-    I = readImage(imgdata);
+    %if no data send 0,0,0 as pose
+    poseCheck = isnan(pose.Translation);
+    pose.Translation(poseCheck) = 0;
 
-    I = undistortImage(I,intrinsics,"OutputView","same");
-
-    [id,loc,pose] = readAprilTag(I,"tag36h11",intrinsics,tagSize);
+    rotCheck = isnan(pose.Rotation);
+    pose.Rotation(rotCheck) = 0;
     
-    posemsg.Position.X = pose.Translation(1);
-    posemsg.Position.Y = pose.Translation(2);
-    posemsg.Position.Z = pose.Translation(3);
+    %populate ros message
+    poseMsg.Position.X = pose.Translation(1);
+    poseMsg.Position.Y = pose.Translation(2);
+    poseMsg.Position.Z = pose.Translation(3);
     quat = rotm2quat(pose.Rotation);
-    posemsg.Orientation.W = quat(1);
-    posemsg.Orientation.X = quat(2);
-    posemsg.Orientation.Y = quat(3);
-    posemsg.Orientation.Z = quat(4);
-    
-    send(posepub, posemsg);
+    poseMsg.Orientation.W = quat(1);
+    poseMsg.Orientation.X = quat(2);
+    poseMsg.Orientation.Y = quat(3);
+    poseMsg.Orientation.Z = quat(4);
+    %publish ros message
+    send(posePub, poseMsg);
     
     pause(0.5);
-
-%     for i = 1:length(pose)
-%         % Get image coordinates for axes.
-%         imagePoints = worldToImage(intrinsics,pose(i).Rotation, ...
-%                       pose(i).Translation,worldPoints);
-% 
-%         % Insert markers to indicate the locations
-%         markerRadius = 8;
-%         numCorners = size(loc,1);
-%         markerPosition = [loc(:,:,i),repmat(markerRadius,numCorners,1)];
-%         I = insertShape(I,"FilledCircle",markerPosition,"Color","red","Opacity",1);
-% 
-%         % Draw colored axes.
-%         I = insertShape(I,"Line",[imagePoints(1,:) imagePoints(2,:); ...
-%             imagePoints(1,:) imagePoints(3,:); imagePoints(1,:) imagePoints(4,:)], ...
-%             "Color",["red","green","blue"],"LineWidth",7);
-% 
-%     %     I = insertText(I,loc(1,:,i),id(i),"BoxOpacity",1,"FontSize",25);
-%     end
+    
  end
